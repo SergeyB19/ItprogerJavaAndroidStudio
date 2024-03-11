0e2000c43c5c445fd50031463a76bbc4d798070c
SergeyB19 <my.h.m@yandex.ru>
2024-03-11 20:08:41 +0300

    Создание калькулятора

--- app/src/main/java/com/itproger/itproger/MainActivity.java
+++ app/src/main/java/com/itproger/itproger/MainActivity.java
@@ -1,6 +1,10 @@
 package com.itproger.itproger;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    private EditText number_field_1;
+    private EditText number_field_2;
+    private Button add_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        resultTextView = findViewById(R.id.resultTextView);
+        number_field_1 = findViewById(R.id.number_field_1);
+        number_field_2 = findViewById(R.id.number_field_2);
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float num1 = Float.parseFloat(number_field_1.getText().toString());
+                float num2 = Float.parseFloat(number_field_2.getText().toString());
+                float res = num1 + num2;
+                resultTextView.setText(String.valueOf(res));
+            }
+        });
+
     }
 }
